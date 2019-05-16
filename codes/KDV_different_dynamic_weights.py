c6c660ae4aeb2aae23e1f398595dc28d9d8b10ee
Zhang Michael <michaelzhang@10-16-236-213.dynapool.wireless.nyu.edu>
2019-05-16 15:06:14 -0400

    raw model

--- codes/KDV_different_dynamic_weights.py
+++ codes/KDV_different_dynamic_weights.py
@@ -90,26 +90,25 @@ class DWPMR:
         self.to_feed_two= tf.placeholder(tf.float32, shape=[None, 1])
 
         # tf graphs for Identification
-        self.ide_u_predict = self.ide_net_u(self.to_feed_t, self.to_feed_x)
+        self.ide_u_predict = self.ide_net_u(self.to_feed_t, self.to_feed_x)[0]
         self.pde_predict = self.net_pde(self.to_feed_terms)
         self.ide_f_predict = self.ide_net_f(self.to_feed_t, self.to_feed_x)[0]
         self.ide_f_ut_predict = self.ide_net_f(self.to_feed_t, self.to_feed_x)[1]
-
         # for regularization
-        self.ide_regular_predict = self.ide_net_f(self.to_feed_t, self.to_feed_x)[2]
+        self.ide_regular_predict = self.ide_net_u(self.to_feed_t, self.to_feed_x)[1]
         # loss for Identification
         self.ide_u_loss = tf.reduce_sum(tf.square(self.ide_u_predict - self.to_feed_u))
 
         # normal weights for f_loss
         # self.idn_f_loss = tf.reduce_sum(tf.square(self.idn_f_pred))
 
-        # dynamic weights with f_loss
+        # dynamic weights with tan for f_loss
         self.ide_f_loss = tf.reduce_sum(tf.multiply(tf.subtract(self.to_feed_one,tf.nn.tanh(tf.square(self.ide_u_predict - self.to_feed_u))),tf.square(self.ide_f_predict)))
-        # logistic
+
+        # dynamic weights with logistic for f_loss
         # self.idn_f_loss = tf.reduce_sum(tf.multiply(tf.multiply(tf.subtract(self.one_tf,tf.nn.sigmoid(tf.square(self.idn_u_pred - self.u_tf))),self.two),tf.square(self.idn_f_pred)))
 
         # regulation item
-        # self.regular_weights = np.full_like(self.idn_regular_pred, 1.0)
         self.regular_weights = tf.reduce_sum(self.to_feed_one)
         self.ide_regular = tf.reduce_sum(tf.square(self.ide_regular_predict)) * 1.0/2.0 * self.regular_weights
 
@@ -148,17 +147,14 @@ class DWPMR:
 
 
         self.ide_u_optimizer_Adam = tf.train.AdamOptimizer()
-        self.ide_u_train_op_Adam = self.ide_u_optimizer_Adam.minimize(self.ide_u_loss,
-                                   var_list = self.u_weights_list + self.u_biases_list)
+        self.ide_u_train_op_Adam = self.ide_u_optimizer_Adam.minimize(self.ide_u_loss,var_list = self.u_weights_list + self.u_biases_list)
 
         self.ide_f_optimizer_Adam = tf.train.AdamOptimizer()
-        self.ide_f_train_op_Adam = self.ide_f_optimizer_Adam.minimize(self.ide_f_loss,
-                                   var_list = self.pde_weights_list + self.pde_biases_list)
+        self.ide_f_train_op_Adam = self.ide_f_optimizer_Adam.minimize(self.ide_f_loss,var_list = self.pde_weights_list + self.pde_biases_list)
 
         # for regularization
         self.ide_regular_optimizer_Adam = tf.train.AdamOptimizer()
-        self.ide_regular_train_op_Adam = self.ide_regular_optimizer_Adam.minimize(self.ide_regular,
-                                   var_list = self.u_weights_list + self.u_biases_list)
+        self.ide_regular_train_op_Adam = self.ide_regular_optimizer_Adam.minimize(self.ide_regular,var_list = self.u_weights_list + self.u_biases_list)
 
 
 
@@ -167,7 +163,13 @@ class DWPMR:
         X = tf.concat([t,x],1)
         H = 2.0*(X - self.lower_bound)/(self.upper_bound - self.lower_bound) - 1.0
         u = set_neural_net(H, self.u_weights_list, self.u_biases_list)
-        return u
+
+        u_t = tf.gradients(u, t)[0]
+        u_x = tf.gradients(u, x)[0]
+        u_xx = tf.gradients(u_x, x)[0]
+        u_xxx = tf.gradients(u_xx, x)[0]
+        regular = u_t+ u*u_x + u_xxx
+        return [u,regular]
 
     def net_pde(self, terms):
         pde = set_neural_net(terms, self.pde_weights_list, self.pde_biases_list)
@@ -176,23 +178,23 @@ class DWPMR:
 
 # second network to learn f
     def ide_net_f(self, t, x):
-        u = self.ide_net_u(t, x)
+        u = self.ide_net_u(t, x)[0]
 
         u_t = tf.gradients(u, t)[0]
 
         u_x = tf.gradients(u, x)[0]
         u_xx = tf.gradients(u_x, x)[0]
+        u_xxx = tf.gradients(u_xx, x)[0]
 
-        terms = tf.concat([u,u_x,u_xx],1)
-
+        terms = tf.concat([u, u_x, u_xx, u_xxx], 1)
         f = u_t - self.net_pde(terms)
-
-        regular = u_t + u * u_x - 0.1*u_xx
+        regular = u_t + u * u_x - 0.1 * u_xx
 
         # for regulation
         return [f,u_t,regular]
 
 
+#  for training two networks
     def ide_regular_train(self, itertime):
         tf_dict = {self.to_feed_t: self.t, self.to_feed_x: self.x, self.to_feed_u: self.u,self.to_feed_one:self.one}
 
@@ -276,55 +278,30 @@ class DWPMR:
 if __name__ == "__main__":
 
     # Doman bounds
-    lb_idn = np.array([0.0, -8.0])
-    ub_idn = np.array([10.0, 8.0])
+    lower_bound = np.array([0.0, -20.0])
+    upper_bound = np.array([40.0, 20.0])
 
-    lb_sol = np.array([0.0, -8.0])
-    ub_sol = np.array([10.0, 8.0])
+    # Load Data from data files
+    data_ide = scipy.io.loadmat('../Data/KdV_sine.mat')
 
-    ### Load Data ###
+    t_ide = data_ide['t'].flatten()[:,None]
+    x_ide = data_ide['x'].flatten()[:,None]
 
-    data_idn = scipy.io.loadmat('../Data/burgers.mat')
+    Exact_ide = np.real(data_ide['usol'])
 
-    t_idn = data_idn['t'].flatten()[:,None]
-    x_idn = data_idn['x'].flatten()[:,None]
-    print(t_idn)
-    print(x_idn)
-    Exact_idn = np.real(data_idn['usol'])
-
-    T_idn, X_idn = np.meshgrid(t_idn,x_idn)
-    print(Exact_idn)
-    print("TTTXXX")
-    print(T_idn)
-    print(X_idn)
+    T_ide, X_ide = np.meshgrid(t_ide,x_ide)
 
     keep = 2/3
-    index = int(keep*t_idn.shape[0])
-    T_idn = T_idn[:,0:index]
-    X_idn = X_idn[:,0:index]
-    Exact_idn = Exact_idn[:,0:index]
+    index = int(keep*t_ide.shape[0])
+    T_ide = T_ide[:,0:index]
+    X_ide = X_ide[:,0:index]
+    Exact_idn = Exact_ide[:,0:index]
 
-    t_idn_star = T_idn.flatten()[:,None]
-    x_idn_star = X_idn.flatten()[:,None]
+    t_idn_star = T_ide.flatten()[:,None]
+    x_idn_star = X_ide.flatten()[:,None]
     X_idn_star = np.hstack((t_idn_star, x_idn_star))
     u_idn_star = Exact_idn.flatten()[:,None]
 
-    #
-
-    data_sol = scipy.io.loadmat('../Data/burgers_sine.mat')
-
-    t_sol = data_sol['t'].flatten()[:,None]
-    x_sol = data_sol['x'].flatten()[:,None]
-    Exact_sol = np.real(data_sol['usol'])
-
-    T_sol, X_sol = np.meshgrid(t_sol,x_sol)
-
-    t_sol_star = T_sol.flatten()[:,None]
-    x_sol_star = X_sol.flatten()[:,None]
-    X_sol_star = np.hstack((t_sol_star, x_sol_star))
-    u_sol_star = Exact_sol.flatten()[:,None]
-
-    ### Training Data ###
 
     # For identification
     N_train = 10000
@@ -334,70 +311,36 @@ if __name__ == "__main__":
     x_train = x_idn_star[idx,:]
     u_train = u_idn_star[idx,:]
 
-    noise = 0.00
+    # adjust the noise
+    noise = 0.05
     u_train = u_train + noise*np.std(u_train)*np.random.randn(u_train.shape[0], u_train.shape[1])
 
-    # # For solution
-    # N0 = Exact_sol.shape[0]
-    # N_b = Exact_sol.shape[1]
-    # N_f = 20000
-    #
-    # idx_x = np.random.choice(x_sol.shape[0], N0, replace=False)
-    # x0_train = x_sol[idx_x,:]
-    # u0_train = Exact_sol[idx_x,0:1]
-    #
-    # idx_t = np.random.choice(t_sol.shape[0], N_b, replace=False)
-    # tb_train = t_sol[idx_t,:]
-    #
-    # X_f_train = lb_sol + (ub_sol-lb_sol)*lhs(2, N_f)
-    #
     # Layers
     u_layers = [2, 50, 50, 50, 50, 1]
-    pde_layers = [3, 100, 100, 1]
-    # our layer
-    # u_layers = [2, 100,100, 100, 100, 1]
-    # pde_layers = [3, 200, 200, 1]
-
-
+    pde_layers = [4, 100, 100, 1]
     layers = [2, 50, 50, 50, 50, 1]
 
-    # Model
-    model = DWPMR(t_train, x_train, u_train,
+    # Main model
+    main_model = DWPMR(t_train, x_train, u_train,
                     u_layers, pde_layers,
-                    lb_idn, ub_idn,
+                    lower_bound, upper_bound,
                     )
     #
     # Train the identifier
-    model.ide_u_train(itertime=0)
+    main_model.ide_u_train(itertime=0)
     #
-    model.ide_f_train(itertime=0)
+    main_model.ide_f_train(itertime=0)
     #
     # model.idn_regular_train(N_iter=0)
 
-    # model.idn_u_f_train(N_iter=0)
-
-    u_pred_identifier, f_pred_identifier = model.idn_predict(t_idn_star, x_idn_star)
+    u_pred_identifier, f_pred_identifier = main_model.ide_predict(t_idn_star, x_idn_star)
 
     error_u_identifier = np.linalg.norm(u_idn_star-u_pred_identifier,2)/np.linalg.norm(u_idn_star,2)
     print('Error u: %e' % (error_u_identifier))
 
-    # ### Solution ###
-    #
-    # # Train the solver
-    # # model.sol_train(N_iter=0)
-    # #
-    # # u_pred, f_pred = model.sol_predict(t_sol_star, x_sol_star)
-    # #
-    # # error_u = np.linalg.norm(u_sol_star-u_pred,2)/np.linalg.norm(u_sol_star,2)
-    # # print('Error u: %e' % (error_u))
-    # #
-    U_pred = griddata(X_idn_star, u_pred_identifier.flatten(), (T_idn, X_idn), method='cubic')
+    U_pred = griddata(X_idn_star, u_pred_identifier.flatten(), (T_ide, X_ide), method='cubic')
 
-    ######################################################################
-    ############################# Plotting ###############################
-    ######################################################################
-    #
-    # fig1 = plt.figure()
+# for plot
     fig, ax = newfig(1.0, 0.6)
     ax.axis('off')
 
@@ -407,7 +350,7 @@ if __name__ == "__main__":
     gs.update(top=0.8, bottom=0.2, left=0.1, right=0.9, wspace=0.5)
     ax = plt.subplot(gs[:, 0])
     h = ax.imshow(Exact_idn, interpolation='nearest', cmap='jet',
-                  extent=[lb_sol[0], ub_sol[0], lb_sol[1], ub_sol[1]],
+                  extent=[lower_bound[0], upper_bound[0], lower_bound[1], upper_bound[1]],
                   origin='lower', aspect='auto')
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="5%", pad=0.05)
@@ -420,7 +363,7 @@ if __name__ == "__main__":
     # ########     Exact p(t,x,y)     ###########
     ax = plt.subplot(gs[:, 1])
     h = ax.imshow(U_pred, interpolation='nearest', cmap='jet',
-                  extent=[lb_sol[0], ub_sol[0], lb_sol[1], ub_sol[1]],
+                  extent=[lower_bound[0], upper_bound[0], lower_bound[1], upper_bound[1]],
                   origin='lower', aspect='auto')
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="5%", pad=0.05)
